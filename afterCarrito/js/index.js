cdafce66517769acf9729a9b5cbc5525b7181933
macarmijo <macca.armijo@gmail.com>
2022-08-15 22:45:14 -0300

    agrego JS

---
+++ afterCarrito/js/index.js
@@ -0,0 +1,56 @@
+const productos = [
+    {
+    id: 1,
+    nombre: "Camara Cannon",
+    precio: 2400,
+    stock: 5,
+    img: 'https://i.ibb.co/3r1VmqY/kisspng-canon-eos-77d-digital-slr-camera-canon-eos-rebel-t-5be1c3af130ce6-668393311541522351078.png'
+    },
+    {
+    id: 2,
+    nombre: "Camara Nikon",
+    precio: 4500,
+    stock: 10,
+    img: 'https://i.ibb.co/tXkCRJ3/5a2306d7e8f110-2152834515122449519541.png'
+    },
+    {
+    id: 3,
+    nombre: "Camara Video JVC",
+    precio: 2800,
+    stock: 50,
+    img: 'https://i.ibb.co/D8nxvbZ/kisspng-video-cameras-1080p-jvc-zoom-lens-binoculars-phone-5ae22b34e3cde0-7991595815247716369331.png'
+    },
+    {
+    id: 4,
+    nombre: "Camara video Sony",
+    precio: 8000,
+    stock: 5,
+    img: 'https://i.ibb.co/zGxkWJG/5a229b502c5419-3296015815122174241816.png'
+    },
+    {
+    id: 5,
+    nombre: "Camara Vintage",
+    precio: 10000,
+    stock: 3,
+    img: 'https://i.ibb.co/LY3KJgr/kisspng-camera-camera-5a92ae871df6e3-2739390515195623751227.png'
+    },
+    {
+    id: 6,
+    nombre: "Camara Polaroid",
+    precio: 9000,
+    stock: 15,
+    img: 'https://i.ibb.co/ZMqmYwh/kisspng-instant-camera-polaroid-corporation-photography-color-polaroid-cameras-vector-material-5aae6.png'
+    }
+]
+
+/* Para el HTML del producto
+`<article class="product">
+            <div class="imagen">
+                <img src="" alt="">
+            </div>
+            <div class="descripcion">
+                <h3></h3>
+                <div class="boton">AGREGAR</div>
+            </div>
+        </article>` 
+*/
